8b952376a51829a6628d200c114f833729fa11be
nperosu <nperosu756@g.educaand.es>
2026-06-26 21:05:50 +0200

    Update SEN0237.ino

--- sensores/SEN0237.ino
+++ sensores/SEN0237.ino
@@ -1,6 +1,3 @@
-/*Código de funcionamiento modificar correspondientes al Wifi y a la etapa en la que se coloca el 
-sensor usa: agua_bruta, floculacion, acondicionado,control_calidad):
-*/
 #include <WiFi.h>
 #include <PubSubClient.h>
 
@@ -13,13 +10,26 @@ sensor usa: agua_bruta, floculacion, acondicionado,control_calidad):
 */
 
 #define DO_PIN 34
+#define ADC_RES 4096
+#define READ_TEMP 25
+#define TWO_POINT_CALIBRATION 0
+#define CAL1_V 1600
+#define CAL1_T 25
+#define CAL2_V 1300
+#define CAL2_T 15
+const uint16_t DO_Table[41] = {
+  14460, 14220, 13820, 13440, 13090, 12740, 12420, 12110, 11810, 11530,
+  11260, 11010, 10770, 10530, 10300, 10080, 9860, 9660, 9460, 9270,
+  9080, 8900, 8730, 8570, 8410, 8250, 8110, 7960, 7820, 7690,
+  7560, 7430, 7300, 7180, 7070, 6950, 6840, 6730, 6630, 6530, 6410
+};
 
 // WiFi
-const char* ssid = "TU_WIFI";
-const char* password = "TU_PASSWORD";
+const char* ssid = "Nombre_WIFI";
+const char* password = "PASSWORD_wifi";
 
 // MQTT
-const char* mqtt_server = "IP_DE_TU_RASPBERRY";
+const char* mqtt_server = "IP_DE_RASPBERRY";
 const int mqtt_port = 1883;
 const char* mqtt_topic = "etap/etapa/oxigeno";
 
@@ -28,6 +38,19 @@ PubSubClient client(espClient);
 
 unsigned long mqttTime = 0;
 const unsigned long MQTT_INTERVAL = 2000;
+
+
+int16_t readDO(uint32_t voltage_mv, uint8_t temperature_c) {
+#if TWO_POINT_CALIBRATION == 0
+  uint16_t V_saturation = (uint32_t)CAL1_V + (uint32_t)35 * temperature_c - (uint32_t)CAL1_T * 35;
+  return (voltage_mv * DO_Table[temperature_c] / V_saturation);
+#else
+  uint16_t V_saturation = (int16_t)((int8_t)temperature_c - CAL2_T) *
+                          ((uint16_t)CAL1_V - CAL2_V) /
+                          ((uint8_t)CAL1_T - CAL2_T) + CAL2_V;
+  return (voltage_mv * DO_Table[temperature_c] / V_saturation);
+#endif
+}
 
 void setup_wifi() {
 WiFi.begin(ssid, password);
@@ -42,7 +65,6 @@ Serial.println(WiFi.localIP());
 
 void reconnect_mqtt() {
 while (!client.connected()) {
-
 if (client.connect("FireBeetle_DO")) {
 Serial.println("MQTT conectado");
 } else {
@@ -52,7 +74,6 @@ delay(5000);
 }
 
 void setup() {
-
 Serial.begin(115200);
 
 analogReadResolution(12);
@@ -74,24 +95,38 @@ client.loop();
 int adc = analogRead(DO_PIN);
 
 float voltage = adc * 3.3 / 4095.0;
+uint16_t voltage_mv = (uint32_t)VREF * adc / ADC_RES;
+
+uint8_t temperature = READ_TEMP;
+
+float oxygen_ugL = readDO(voltage_mv, temperature);
+float oxygen_mgL = oxygen_ugL / 1000.0;
 
 Serial.print("ADC: ");
 Serial.print(adc);
-
 Serial.print(" | Voltaje: ");
 Serial.print(voltage, 3);
-Serial.println(" V");
+Serial.print(" V | Oxigeno: ");
+Serial.print(oxygen_mgL, 2);
+Serial.println(" mg/L");
 
 if (millis() - mqttTime > MQTT_INTERVAL) {
 
 char payload[200];
 
 snprintf(payload, sizeof(payload),
-"{\"sensor\":\"SEN0237-A\","
-"\"parametro\":\"oxigeno_disuelto\","
-"\"voltaje\":%.3f,"
-"\"unidad\":\"V\"}",
-voltage);
+      "{\"sensor\":\"SEN0237-A\","
+      "\"parametro\":\"oxigeno_disuelto\","
+      "\"valor\":%.2f,"
+      "\"unidad\":\"mg/L\","
+      "\"voltaje\":%.3f,"
+      "\"unidad_voltaje\":\"V\","
+      "\"temperatura\":%d}",
+      oxygen_mgL,
+      voltage,
+      temperature
+    );
+
 
 client.publish(mqtt_topic, payload);
 
@@ -100,7 +135,5 @@ mqttTime = millis();
 
 delay(1000);
 }
-
-
 
 
